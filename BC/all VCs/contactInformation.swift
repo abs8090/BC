ef567d7fff02ac71f49dc81fdea111b0c6db33a3
abs8090 <km.5432@hotmail.com>
2018-04-27 00:22:04 -0400

    ready for presentation

---
+++ BC/all VCs/contactInformation.swift
@@ -0,0 +1,9 @@
+//
+//  contactInformation.swift
+//  BC
+//
+//  Created by Abdulaziz Alhomaidhi on 4/26/18.
+//  Copyright © 2018 Abdulaziz Alhomaidhi. All rights reserved.
+//
+
+import Foundation
